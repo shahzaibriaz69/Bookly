5d5c7d2a7ce828c965c1df42f3fef597682d12b5
shahzaibriaz69 <shahzaibriaz0023@gmail.com>
2025-11-12 16:20:42 +0500

    cart drop complete and also update whishlist

--- custom.js
+++ custom.js
@@ -17,3 +17,32 @@ wishlist.addEventListener("click", function (e) {
   wishlist_menu.style.display =
     wishlist_menu.style.display === "block" ? "none" : "block";
 })
+
+let cart = document.querySelector("#cart");
+let main_cart = document.querySelector(".main-cart");
+
+cart.addEventListener("click", function (e) {
+  e.preventDefault();
+  cart.classList.toggle("active");
+  main_cart.style.display =
+    main_cart.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", function (e) {
+  // Dropdown
+  if (!pages_text.contains(e.target) && !dropdown_menu.contains(e.target)) {
+    dropdown_menu.style.display = "none";
+  }
+
+  // Wishlist
+  if (!wishlist.contains(e.target) && !wishlist_menu.contains(e.target)) {
+    wishlist_menu.style.display = "none";
+    wishlist.classList.remove("active");
+  }
+
+  // Cart
+  if (!cart.contains(e.target) && !main_cart.contains(e.target)) {
+    main_cart.style.display = "none";
+    cart.classList.remove("active");
+  }
+});
